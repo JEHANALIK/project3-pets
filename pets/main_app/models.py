07771e18d6b2a0838a413602b7dcc16992993e66
Hajar Mohamed <hajr.m7md@gmail.com>
2023-03-16 15:07:53 +0300

    added Service model- base.html,home.html group work @zahidala @jehanalik @nadia-husain

--- pets/main_app/models.py
+++ pets/main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Service(models.Model):
+    name= models.CharField(max_length=100)
+    description= models.TextField(max_length=250)
+    price= models.FloatField()
+    image= models.ImageField(upload_to='main_app/static/uploads', blank=True)
+    
+
