2f8e0897b9aa3053384dc2947b45923ff1f70cea
Nadia Husain <nadia.neerooz@gmail.com>
2023-03-18 11:39:53 +0300

    created pets model

--- pets/main_app/models.py
+++ pets/main_app/models.py
@@ -16,3 +16,13 @@ class Appointments(models.Model):
     
     class Meta:
         ordering = ['-date']
+
+class Pets(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    age = models.IntegerField()
+    description = models.TextField(max_length=300)
+    image = models.ImageField(upload_to='main_app/static/uploads', blank=True)
+    appointments = models.ForeignKey(Appointments, on_delete=models.CASCADE)
+    
