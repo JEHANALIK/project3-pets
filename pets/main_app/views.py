e46cb8cfaa0ac0322fb20c853c14fd16fb4b950a
Nadia Husain <nadia.neerooz@gmail.com>
2023-03-19 16:27:31 +0300

    appointment solved

--- pets/main_app/views.py
+++ pets/main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Service , Appointments, Pets
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
